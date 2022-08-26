982cc134913f32c13792e0fb3e8fd0e4df6547da
Neel Tangella <neel.tangella20@gmail.com>
2022-08-25 20:02:29 -0700

    inverse transform added, this is what we are gonna want to use

--- levy-dist.h
+++ levy-dist.h
@@ -1,8 +1,62 @@
 #include <random>
 #include <cmath>
-#include <array> 
+#include <array>
+#include <stdexcept> 
+#include <iostream>
 
 
+//Code lifted from John Cook, Ph.D.
+double rational_approximation(double t){
+
+	//Abramowitz and Stegun formula
+	double c[] = {2.515517, 0.802853, 0.010328}; 
+	double d[] = {1.432788, 0.189269, 0.001308}; 
+
+	return t - ((c[2]*t + c[1])*t + c[0]) / (((d[2]*t + d[1]*t) + d[0]) *t + 1.0);; 
+}
+
+double inv_norm_cdf(double p){
+
+	if (p < 0.0 || p > 1.0){
+		std::cout << p << std::endl; 
+		throw std::invalid_argument("input must be greater than 0 and less than 1"); 
+	}
+	if (p < 0.5) return -rational_approximation(sqrt(-2.0*log(p))); 
+	else return rational_approximation(sqrt(-2.0*log(1-p))); 
+}
+
+//End code lifted from John Cook//
+
+
+//inverse transform sampling
+double get_levy_inverse_transform(double c=0.5, double mu=0, bool truncate=false, double truncate_val=1.0){
+
+	std::random_device rd; 
+	std::mt19937 e2(rd());
+
+	std::uniform_real_distribution<> var(0,1); 
+	double res = 0.0; 
+
+	if(truncate){
+		do {
+			double u = var(e2); 
+			double n = c; 
+			double d = pow(inv_norm_cdf(1-u), 2);
+			res = n/d + mu; 
+		} 
+		while(res > truncate_val); 
+	}
+	else{
+		double u = var(e2); 
+		double n = c; 
+		double d = pow(inv_norm_cdf(1-u), 2);
+		res = n/d + mu; 
+	}
+	return res;  
+}
+
+
+//rejection sampling
 double get_levy(double c=0.5, double mu=0.5, double x_low=1, double x_high=1.5, double y_low=0, double y_high=1.5){
 
 	//Rejection Sampling: 
@@ -40,7 +94,9 @@ double get_levy(double c=0.5, double mu=0.5, double x_low=1, double x_high=1.5, 
 
 }
 
-double get_levy_test(std::array<double, 2>& params, double c=0.5, double mu=0.5, double x_low=1, double x_high=1.5, double y_low=0, double y_high=1.5){
+
+//Test function for rejection sampling from a Lévy distribution
+double get_levy_test(std::array<double, 2>& params, double c=0.5, double mu=0., double x_low=1, double x_high=1.5, double y_low=0, double y_high=1.5){
 
 	//Rejection Sampling: 
 
--- levy-test.py
+++ levy-test.py
@@ -12,15 +12,16 @@ params = pd.read_csv(myfile_params, header=None)
 c = params[0]
 mu = params[1]
 
-c = 1
+c = 0.5
+mu=0
 
 #work in progress what id like to have is the equation plotted alongisde the density plot
-pdf = lambda x: np.sqrt(c/np.pi) * (np.exp(-1 * (c/(2 * (x-mu)))) / np.power((x-mu), 1.5))
+pdf = lambda x: np.sqrt(c/np.pi) * (np.exp(-1 * (c/(2 * (x)))) / np.power((x), 1.5))
 
 
 sample_high = dat.max()
-x_low = 1.0
-x_high = 1.5
+x_low = 1e-3
+x_high = 1
 
 x = np.linspace(x_low, x_high, 1000)
 y = np.empty(0,dtype=float)
@@ -31,18 +32,19 @@ for num in x:
     
 n_bins = 100
 
+
 ax1 = plt.subplot(2, 1, 1)
 ax2 = plt.subplot(2, 1, 2)
 ax1.hist(dat, bins=n_bins)
-ax1.set_ylabel("Frequency")
+ax1.set_ylabel("Sample Frequency")
 ax1.set_xlim(left=0)
 
 # ax1.set_xlabel("Variate")
 ax1.set_title("Lévy Distribution Sample Density Plot")
-ax2.plot(x, y)
+ax2.plot(x, y, 'b')
 ax2.set_xlim(left=x_low)
 # ax2.set_title("Lévy Distribution PDF")
-ax2.set_ylabel("Density")
+ax2.set_ylabel("Probability Density")
 ax2.set_xlabel("Variate")
 
 plt.show()
